bc273845476b39beb3f1e26e2156990e6da7a2d5
LuizGustavo <gustavol2010@yahoo.com>
2022-01-21 14:26:39 -0300

    3

--- EstruturaHTML/js/script.js
+++ EstruturaHTML/js/script.js
@@ -3,6 +3,8 @@ let navbar = document.querySelector('.navbar')
 document.querySelector('#menu-btn').onclick = () =>{
 
 navbar.classList.toggle('active');
+searchForm.classList.remove('active');
+cartItem.classList.remove('active');
 
 }
 
@@ -13,6 +15,8 @@ let searchForm = document.querySelector('.search-form')
 document.querySelector('#search-btn').onclick = () =>{
 
 searchForm.classList.toggle('active');
+navbar.classList.remove('active');
+cartItem.classList.remove('active');
 
 }
 
@@ -23,5 +27,16 @@ let cartItem = document.querySelector('.cart-items-container')
 document.querySelector('#cart-btn').onclick = () =>{
 
 cartItem.classList.toggle('active');
+navbar.classList.remove('active');
+searchForm.classList.remove('active');
+
+
+}
+
+window.onscroll = () => { 
+
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
 
 }
