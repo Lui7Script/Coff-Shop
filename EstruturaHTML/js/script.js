3dffe0dcfe728d18c6ca4269daf87c905b3e53d5
LuizGustavo <gustavol2010@yahoo.com>
2022-01-21 13:59:56 -0300

    CoffSHop2

---
+++ EstruturaHTML/js/script.js
@@ -0,0 +1,25 @@
+let navbar = document.querySelector('.navbar')
+
+document.querySelector('#menu-btn').onclick = () =>{
+
+navbar.classList.toggle('active');
+
+}
+
+
+let searchForm = document.querySelector('.search-form')
+
+document.querySelector('#search-btn').onclick = () =>{
+
+searchForm.classList.toggle('active');
+
+}
+
+
+let cartItem = document.querySelector('.cart-items-container')
+
+document.querySelector('#cart-btn').onclick = () =>{
+
+cartItem.classList.toggle('active');
+
+}
