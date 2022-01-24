48017bfb33994d54dd054cbef0f038775fd0abc9
LuizGustavo <gustavol2010@yahoo.com>
2022-01-24 09:22:11 -0300

    CoffContinuação

--- EstruturaHTML/js/script.js
+++ EstruturaHTML/js/script.js
@@ -9,7 +9,6 @@ cartItem.classList.remove('active');
 }
 
 //
-
 let searchForm = document.querySelector('.search-form')
 
 document.querySelector('#search-btn').onclick = () =>{
@@ -17,11 +16,9 @@ document.querySelector('#search-btn').onclick = () =>{
 searchForm.classList.toggle('active');
 navbar.classList.remove('active');
 cartItem.classList.remove('active');
-
 }
 
 //
-
 let cartItem = document.querySelector('.cart-items-container')
 
 document.querySelector('#cart-btn').onclick = () =>{
@@ -29,8 +26,6 @@ document.querySelector('#cart-btn').onclick = () =>{
 cartItem.classList.toggle('active');
 navbar.classList.remove('active');
 searchForm.classList.remove('active');
-
-
 }
 
 window.onscroll = () => { 
