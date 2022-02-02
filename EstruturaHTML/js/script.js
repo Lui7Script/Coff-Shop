a1a3d03f8cf09651402ef14b8bc3eea23e467697
LuizGustavo <gustavol2010@yahoo.com>
2022-02-02 11:49:23 -0300

    Footer

--- EstruturaHTML/js/script.js
+++ EstruturaHTML/js/script.js
@@ -1,37 +1,35 @@
 let navbar = document.querySelector('.navbar')
 
-document.querySelector('#menu-btn').onclick = () =>{
+document.querySelector('#menu-btn').onclick = () => {
 
-navbar.classList.toggle('active');
-searchForm.classList.remove('active');
-cartItem.classList.remove('active');
-
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
 }
 
 //
 let searchForm = document.querySelector('.search-form')
 
-document.querySelector('#search-btn').onclick = () =>{
+document.querySelector('#search-btn').onclick = () => {
 
-searchForm.classList.toggle('active');
-navbar.classList.remove('active');
-cartItem.classList.remove('active');
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
 }
 
 //
 let cartItem = document.querySelector('.cart-items-container')
 
-document.querySelector('#cart-btn').onclick = () =>{
+document.querySelector('#cart-btn').onclick = () => {
 
-cartItem.classList.toggle('active');
-navbar.classList.remove('active');
-searchForm.classList.remove('active');
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
 }
 
- window.onscroll = () => { 
+window.onscroll = () => {
 
     navbar.classList.remove('active');
     searchForm.classList.remove('active');
     cartItem.classList.remove('active');
-
 }
